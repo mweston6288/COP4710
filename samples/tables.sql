caf1eba6a39d80e73e3f38737471062870942998
mweston6288 <mweston6288@knights.ucf.edu>
2021-11-20 08:50:02 -0500

    Fixed up the table so admins and professors have unique IDs and books is a separate table

--- samples/tables.sql
+++ samples/tables.sql
@@ -6,27 +6,34 @@ use db;
 create table if not exists admin (
     username varchar(100),
     password varchar(100),
+    updateNeeded bool,
     primary key(username)
-
 );
-create table if not exists request( -- make one entry for each specific book request?
-    reqId integer,-- primary key?
+
+create table if not exists book(
     bookTitle varchar(100),
     author varchar(100),
     edition real,
     publisher varchar(100),
     ISBN integer,-- primary key?
+    primary key (ISBN)
+);
+create table if not exists request( -- make one entry for each specific book request?
+    profId integer,-- primary key?
     semester varchar(100),
-    primary key (reqId, ISBN)
+    ISBN integer,-- primary key?    
+    primary key (profId, ISBN, semester)
+    foreign key (profId) references professor(profId),
+    foreign key (ISBN) references book(ISBN)
 );
 create table if not exists professor(
     email varchar(100),-- likely primary key
     name varchar(100),
     username varchar(100),-- can be blank, blank triggers account creation
     password varchar(100),
-    reqId integer,
+    profId integer,
+    updateNeeded bool,
     primary key(email),
-    foreign key (reqid) references request(reqid)
 );
 -- default entries
 insert into admin(username, password) values ("admin", "password");
---
+++ tables.sql
@@ -0,0 +1,43 @@
+-- When opening mysql from terminal, run "mysql -u root -p < [path to tables.sql]", 
+
+create database if not exists db;
+use db;
+
+create table if not exists admin (
+	adminId integer not null auto_increment,
+    username varchar(100) not null,
+    password varchar(100) not null,
+    -- useless for now, but will use if we implement a force password reset
+    updateNeeded bool, 
+    primary key(adminId)
+);
+
+create table if not exists book(
+    bookTitle varchar(100) not null,
+    author varchar(100) not null,
+    edition real not null,
+    publisher varchar(100) not null,
+    ISBN integer not null,
+    primary key (ISBN)
+);
+create table if not exists professor(
+    profId integer not null auto_increment,    
+    email varchar(100) not null,
+    name varchar(100) not null,
+    username varchar(100),-- can be blank, blank triggers account creation
+    password varchar(100),
+    -- useless for now, but will use if we implement a force password reset
+    updateNeeded bool,
+    primary key(profId)
+);
+-- tracks each book request by each professor in each semester
+create table if not exists request(
+    profId integer not null,
+    semester varchar(100) not null,
+    ISBN integer not null,    
+    primary key (profId, ISBN, semester)
+    foreign key (profId) references professor(profId),
+    foreign key (ISBN) references book(ISBN)
+);
+-- default entries
+insert into admin(username, password) values ("admin", "password");
